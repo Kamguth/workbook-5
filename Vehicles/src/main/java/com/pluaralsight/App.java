2350cc0c1b073b40007d9a82a99ac6750bbd789f
Kamron Guthrie <kamrontguthrie@gmail.com>
2025-05-12 14:35:12 -0400

    Exercise complete

--- Vehicles/src/main/java/com/pluaralsight/App.java
+++ Vehicles/src/main/java/com/pluaralsight/App.java
@@ -3,13 +3,13 @@ package com.pluaralsight;
 
 public class App {
     public static void main(String[] args) {
-        Moped slowRide = new Moped();
+        /*Moped slowRide = new Moped();
         slowRide.setColor("Brown");
         slowRide.setFuelCapacity(5);
         slowRide.setNumberOfPassengers(2);
         slowRide.setCargoCapacity(10);
 
-        Car fastRide = new Car();
+       Car fastRide = new Car();
         fastRide.setColor("Grey");
         fastRide.setFuelCapacity(15);
         fastRide.setNumberOfPassengers(4);
@@ -25,9 +25,17 @@ public class App {
         theJetsons.setColor("Red");
         theJetsons.setFuelCapacity(100);
         theJetsons.setNumberOfPassengers(4);
-        theJetsons.setCargoCapacity(35);
+        theJetsons.setCargoCapacity(35);*/
+
+        Moped slowRide = new Moped("Brown", 2, 10, 5);
+        Car fastRide = new Car("Grey", 4, 30, 15);
+        SemiTruck tanker = new SemiTruck("White", 2, 500, 80);
+        Hovercraft theJetsons = new Hovercraft("Red", 4, 35, 100);
 
         System.out.println("The Hovercraft is " + theJetsons.getColor() + " and it can hold " + theJetsons.getNumberOfPassengers() + " people.");
+        System.out.println("The Semi Truck is " + tanker.getColor() + " and it can hold " + tanker.getNumberOfPassengers() + " people.");
+        System.out.println("The car is " + fastRide.getColor() + " and it can hold " + fastRide.getNumberOfPassengers() + " people.");
+        System.out.println("The moped is " + slowRide.getColor() + " and it can hold " + slowRide.getNumberOfPassengers() + " people.");
 
     }
 }
